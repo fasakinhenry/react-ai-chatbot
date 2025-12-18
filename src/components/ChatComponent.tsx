de08c406aaf6fbeef5c69806e8f3916e29c393b4
Fasakin Henry <fasakinhenry@gmail.com>
2025-12-18 18:12:33 +0100

    feat: create the User Interface of the Chatbot

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react';
+import { LuBot, LuSendHorizontal } from 'react-icons/lu';
 
 const ChatComponent: React.FunctionComponent = () => {
+  const [input, setInput] = React.useState<string>('');
+  const handleSend = () => {
+    // Handle send action
+    console.log('Message sent:', input);
+    setInput('');
+  };
   return (
     <div className='flex flex-col h-[80vh] bg-white'>
-      <h2 className='p-4 font-semibold text-lg text-center flex justify-center items-center gap-2 bg-blue-100 text-blue-800'>React + OpenAI chatbot </h2>
+      <h2 className='p-4 font-semibold text-lg text-center flex justify-center items-center gap-2 bg-blue-100 text-blue-800'>
+        React + OpenAI chatbot <LuBot size={25} />{' '}
+      </h2>
+      <div className="flex-1 overflow-y-auto p-4 space-y-2"></div>
+      <div className='flex items-center p-4 bg-gray-50'>
+        <input
+          type='text'
+          className='flex-1 p-2 border rounded-lg focus:outline-none'
+          placeholder='Your message Here'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleSend} className='p-2'>
+          <LuSendHorizontal size={25} />
+        </button>
+      </div>
     </div>
   );
 };
