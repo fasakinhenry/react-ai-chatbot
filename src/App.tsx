72ce4de300fa190a323c434ce40991e99dfa475f
Fasakin Henry <fasakinhenry@gmail.com>
2025-12-18 10:16:35 +0100

    feat: add header and basic styling for the chatbot component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import ChatComponent from './components/ChatComponent';
 
 const App: React.FunctionComponent = () => {
   return (
--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
 const ChatComponent: React.FunctionComponent = () => {
-  return <div>ChatComponent</div>;
+  return (
+    <div className='flex flex-col h-[80vh] bg-white'>
+      <h2 className='p-4 font-semibold text-lg text-center flex justify-center items-center gap-2 bg-blue-100 text-blue-800'>React + OpenAI chatbot </h2>
+    </div>
+  );
 };
 
 export default ChatComponent;
